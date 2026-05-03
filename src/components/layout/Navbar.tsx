f6256d5bf10142cf0bea65371644f427672c139f
Lior <liorma6@gmail.com>
2026-05-03 14:41:29 +0300

    Make logo much bigger (h-32)

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -8,7 +8,7 @@ export function Navbar() {
           <img
             src="/logo-panacom.png"
             alt="Panacom Logo"
-            className="h-24 w-auto"
+            className="h-32 w-auto"
           />
         </div>
         <a
